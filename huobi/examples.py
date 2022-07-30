e0c1dc6409663fea4c32c610782c05132b3d8532
Olim <aoulacode@gmail.com>
2022-07-30 15:39:11 +0500

    fixes

--- huobi/examples.py
+++ huobi/examples.py
@@ -3,30 +3,30 @@ from huobi import Huobi
 huobi = Huobi('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 # Get all accounts list
-accounts = ftx.get_accounts()
+accounts = huobi.get_accounts()
 
 if accounts.get('status') == 'ok':
     spot_account = list(filter(lambda i: i['type'] == 'spot', accounts.get('data')))[0]
 
-    ftx.change_account_id(spot_account.get('id'))
+    huobi.change_account_id(spot_account.get('id'))
 
 # Get open orders
-open_orders = ftx.get_open_orders()
+open_orders = huobi.get_open_orders()
 
 # Cancel order by id
-cancel_response = ftx.cancel_order('596827573398451')
+cancel_response = huobi.cancel_order('596827573398451')
 
 # Get symbols list
-symbols = ftx.get_symbols()
+symbols = huobi.get_symbols()
 
 # Get klines list
-klines = ftx.get_klines('btcusdt')
+klines = huobi.get_klines('btcusdt')
 
 # Get balances
-balances = ftx.get_balances()
+balances = huobi.get_balances()
 
 # Place limit order
-limit_order_response = ftx.place_order(
+limit_order_response = huobi.place_order(
     amount=101,
     symbol='trxusdt',
     price=0.05,
@@ -34,7 +34,7 @@ limit_order_response = ftx.place_order(
 )
 
 # Place marker order
-market_order_response = ftx.place_order(
+market_order_response = huobi.place_order(
     amount=1,
     symbol='trxusdt',
     type='buy-market'
